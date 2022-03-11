e4ebc797518faca0f05691cce57bf16e6c27e7d4
Huy Duong <huy.duongdinh@gmail.com>
2022-03-11 23:26:58 +0700

    Handle call button

--- simple-peer-to-peer/client/src/components/VideoCall.js
+++ simple-peer-to-peer/client/src/components/VideoCall.js
@@ -11,10 +11,34 @@ const VideoCall = (props) => {
     `ws://localhost:8080/connect?username=${username}`
   );
 
+  webSocketRef.current.addEventListener("message", async (e) => {
+    const message = JSON.parse(e.data);
+    if (!message) {
+      return console.log('Failed to parse msg')
+    }
+
+    switch (message.event) {
+      case "call":
+        console.log(message.data)
+        break;
+      case "message":
+        alert(message.data)
+        break;
+    }
+  });
+
+  const handleCall = async (e) => {
+    e.preventDefault();
+
+    webSocketRef.current.send(JSON.stringify({
+      event: "call", data: '{ "caller": "' + username + '", "callee": "' + anotherUsername + '" }'
+    }));
+  };
+
   return (
     <div>
       <h1>{username}</h1>
-      <button>Call {anotherUsername}</button>
+      <button onClick={handleCall}>Call {anotherUsername}</button>
       <h3> Local Video </h3>
       <video id="localVideo" width="160" height="120" autoPlay muted></video> <br />
 
