334a16b5e50ae2a31a7867346877e1ca3dd3e475
Huy Duong <huy.duongdinh@gmail.com>
2022-03-11 23:23:22 +0700

    Handle call message

--- simple-peer-to-peer/server/main.go
+++ simple-peer-to-peer/server/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var webSocketUsersMap = make(map[string]*websocket.Conn)
+
 func main() {
 	http.HandleFunc("/connect", connectHandler)
 
@@ -28,35 +30,70 @@ type websocketMessage struct {
 	Data  string `json:"data"`
 }
 
+type callMessage struct {
+	Caller string `json:"caller"`
+	Callee string `json:"callee"`
+}
+
 func connectHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Content-Type", "application/json")
 
-  username, ok := r.URL.Query()["username"]
-  if !ok {
-    return
-  }
+	username, ok := r.URL.Query()["username"]
+	if !ok {
+		return
+	}
 
-  log.Println("Login with username " + username[0])
+	log.Println("Login with username " + username[0])
 
-  websocket, err := upgrader.Upgrade(w, r, nil)
-  if err != nil {
-    return
-  }
-  defer websocket.Close()
+	websocket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	webSocketUsersMap[username[0]] = websocket
 
-  message := &websocketMessage{}
-  for {
-    _, raw, err := websocket.ReadMessage()
-    if err != nil {
-      log.Println(err)
-      return
-    }
+	defer func() {
+		websocket.Close()
+		webSocketUsersMap[username[0]] = nil
+	}()
 
-    if err := json.Unmarshal(raw, &message); err != nil {
-      log.Println(err)
-      return
-    }
-  }
+	message := &websocketMessage{}
+	for {
+		_, raw, err := websocket.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if err := json.Unmarshal(raw, &message); err != nil {
+			log.Println(err)
+			return
+		}
+
+		switch message.Event {
+		case "call":
+			callMessage := &callMessage{}
+			if err := json.Unmarshal([]byte(message.Data), &callMessage); err != nil {
+				return
+			}
+
+			if calleeWebsocket, ok := webSocketUsersMap[callMessage.Callee]; ok {
+				log.Println(callMessage.Caller + " call to " + callMessage.Callee)
+
+				calleeWebsocket.WriteJSON(&websocketMessage{
+					Event: "call",
+					Data:  callMessage.Caller,
+				})
+			} else {
+				log.Println(callMessage.Callee + " is not online")
+
+				webSocketUsersMap[callMessage.Caller].WriteJSON(&websocketMessage{
+					Event: "message",
+					Data:  "User " + callMessage.Callee + " is not online",
+				})
+			}
+
+			break
+		}
+	}
 }
-
